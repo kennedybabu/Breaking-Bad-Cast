99e95acf3974f1653ccc840261db7fefaf52649b
kennedybabu <kennedynjogu8@gmail.com>
2022-06-05 13:06:05 +0300

    feature: fetch items using axios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import Header from './components/UI/Header';
+import axios from 'axios';
 
-function App() {
+
+const App = () => {
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(`https://www.breakingbadapi.com/api/characters`)
+
+      console.log(result.data)
+    }
+    fetchItems()
+  }, [])
+
   return (
-    <div>
+      <div>
         
-    </div>
-  );
+      </div>
+  )
 }
-
 export default App;
